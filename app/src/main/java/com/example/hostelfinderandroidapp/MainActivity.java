a96ba67f36679a6b6deaaed1e47b09f516153af9
MHamzaShahzad <hamzashahzad604@gmail.com>
2019-08-21 19:04:56 +0500

    User authentication implemented!

--- app/src/main/java/com/example/hostelfinderandroidapp/MainActivity.java
+++ app/src/main/java/com/example/hostelfinderandroidapp/MainActivity.java
@@ -1,14 +1,128 @@
 package com.example.hostelfinderandroidapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+
+    private static final int RC_SIGN_IN = 1;
+    List<AuthUI.IdpConfig> providers;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (isAlreadySignedIn())
+            startHomeActivity();
+        else {
+            initProviders();
+            showSignInOptions();
+        }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Log.e(TAG, "onActivityResult: VALID_REQUEST_CODE");
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                Log.e(TAG, "onActivityResult: VALID_RESULT_CODE");
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null) {
+
+                    startHomeActivity();
+
+                }
+            } else {
+                if (response != null && response.getError() != null) {
+                    Log.e(TAG, "onActivityResult: Error" + response.getError().getErrorCode());
+                    Toast.makeText(MainActivity.this, "Error : "+ response.getError().getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    private void initProviders() {
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build()
+        );
+    }
+
+    private void showSignInOptions() {
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setIsSmartLockEnabled(false)
+                        .setTheme(R.style.MyTheme)
+                        .build(),
+                RC_SIGN_IN);
+
+    }
+
+    private boolean isAlreadySignedIn() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        return user != null;
+    }
+
+    private void startHomeActivity() {
+        startActivity(new Intent(MainActivity.this, DrawerMainActivity.class));
+        finish();
+    }
+
+    private void getHashKey() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    getPackageName(),
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest messageDigest = MessageDigest.getInstance("SHA");
+                messageDigest.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(messageDigest.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
